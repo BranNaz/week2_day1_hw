7b67d93c1e4e5685d4ecbd919e37ae9335b663e6
Brandon Nazario <brandon.nazario21@gmail.com>
2022-07-08 01:12:39 -0400

    week 2 day 1 hw assign

---
+++ excercise1.py
@@ -0,0 +1,5 @@
+num6=0
+while num6 < 1001:
+    print(num6)
+    num6 += 1
+print('done')
---
+++ excercise3.py
@@ -0,0 +1,7 @@
+age = input('What is your age? ')
+if age < '18': 
+    print('Kid')
+elif age > '65':
+    print('Senior')
+else:
+    print('Adult')
